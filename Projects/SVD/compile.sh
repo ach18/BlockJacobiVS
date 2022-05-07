191a7f7528ce59dd8cd567a5ac85f03d46aed8d7
ach18 <mail@mai.ru>
2022-05-07 19:06:10 +0300

    Изменение флагов компиляции (имена методов)

--- Projects/SVD/compile.sh
+++ Projects/SVD/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DEFINED_FUNCTIONS="-DPRRBJRS -DRRBNSVD -DPRRBNSVD -DRRBNSVD_AVX -DPRRBNSVD_AVX"
+DEFINED_FUNCTIONS="-DPRRBJRS -DCOLBNSVD -DPRRBNSVD -DCOLBNSVD_AVX -DPRRBNSVD_AVX"
 FLAGS="${DEFINED_FUNCTIONS} -DMKL_ILP64 -DMKL_INT=size_t -L${MKLROOT}/lib/intel64 -lmkl_intel_ilp64 -lmkl_intel_thread -lmkl_core -qopenmp -lm -ldl -std=c++11"
 INCLUDE="-I${MKLROOT}/include"
 SRC_FILES="main.cpp ./src/utils/util.cpp ./src/utils/matrix.cpp ./src/utils/block.cpp ./src/svd/one-sided/svd.cpp ./src/svd/two-sided/nsvd.cpp ./src/svd/two-sided/svd_subprocedure.cpp ./src/svd/two-sided/svd_blocked.cpp"
@@ -7,7 +7,7 @@ COMP_FILE=main
 
 # icpc main.cpp ./src/utils/util.cpp ./src/utils/matrix.cpp ./src/utils/block.cpp ./src/svd/one-sided/svd.cpp \
 #./src/svd/two-sided/nsvd.cpp ./src/svd/two-sided/svd_subprocedure.cpp ./src/svd/two-sided/svd_blocked.cpp \
-#-DMKL_INT=size_t -DMKL_ILP64 -DPRRBJRS -DRRBNSVD -DPRRBNSVD -DRRBNSVD_AVX -DPRRBNSVD_AVX \
+#-DMKL_INT=size_t -DMKL_ILP64 -DPRRBJRS -DCOLBNSVD -DPRRBNSVD -DCOLBNSVD_AVX -DPRRBNSVD_AVX \
 #-L${MKLROOT}/lib/intel64 -lmkl_intel_ilp64 -lmkl_intel_thread -lmkl_core -liomp5 -lpthread -lm -ldl -std=c++11 -qopenmp  \
 #-I${MKLROOT}/include \
 #-o main
